0eb6662f84f7fce2d6792b009d5105a106c72f28
Afia Siaw <afiasiaw@gmail.com>
2020-07-23 12:21:49 -0400

    enabled cors

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const app = express();
 
 const db = {
@@ -24,6 +25,7 @@ const db = {
 };
 
 app.use(bodyParser.json());
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.json(db.users);
